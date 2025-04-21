537da68482e888953e14c5bf280157e55fff7528
Omkar Prabhu <prabhuomkar@pm.me>
2025-04-21 22:13:20 +0530

    Updated gitignore

--- examples/mobilebert-fill-mask/main.cc
+++ examples/mobilebert-fill-mask/main.cc
@@ -48,7 +48,7 @@ int main(int argc, char* argv[]) {
   int mask_index = -1;
   at::Tensor input_row = input_ids[0];
   auto input_accessor = input_row.accessor<int64_t, 1>();
-  for (int i = 0; i < input_accessor.size(0); ++i) {
+  for (int i = 0; i < input_accessor.size(0); i++) {
     if (input_accessor[i] == 103) {
       mask_index = i;
       break;
@@ -60,7 +60,7 @@ int main(int argc, char* argv[]) {
   at::Tensor topk_scores = std::get<0>(topk);
   at::Tensor topk_indices = std::get<1>(topk);
   std::cout << "Top " << top_k << " predictions for [MASK] token:" << std::endl;
-  for (int i = 0; i < top_k; ++i) {
+  for (int i = 0; i < top_k; i++) {
     int token_id = topk_indices[i].item<int>();
     float score = topk_scores[i].item<float>();
     auto token = tokenizer.Decode({token_id});
